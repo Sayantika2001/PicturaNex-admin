d80977c53e8622e87cd975c6c28e81036e4e5526
Sayantika Manna <mannasayantika204@gmail.com>
2024-06-04 19:27:38 +0530

    first commit

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -26,7 +26,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://picturanex-backend.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -37,7 +37,7 @@ const AddProduct = () => {
             {
                 product.image = responseData.image_url;
                 console.log(product);
-                await fetch('http://localhost:4000/addproduct',{
+                await fetch('https://picturanex-backend.onrender.com/addproduct',{
                     method:'POST',
                     headers:{
                         Accept:'application/json',
